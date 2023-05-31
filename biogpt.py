e04915d03f9c9a0c791bf07281102e98e1bcff98
sriharshabarkuru <sriharsha.barkuru@oneconvergence.com>
2023-05-31 15:56:16 +0530

    adding biogpt deployment

---
+++ biogpt.py
@@ -0,0 +1,40 @@
+from transformers import pipeline
+import torch
+import json
+from ray import serve
+import os
+
+
+@serve.deployment(ray_actor_options={"num_cpus": 1}, health_check_timeout_s=600)
+class BioGpt:
+    def __init__(self):
+        #self.pipe_biogpt = pipeline("text-generation", model="/model/biogpt")
+        print ("model path", os.environ["MODEL_PATH"])
+        self.pipe_biogpt = pipeline("text-generation", model=os.environ["MODEL_PATH"])
+
+        # print(f"Is CUDA available: {torch.cuda.is_available()}")
+        # print(f"CUDA device: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+
+    async def __call__(self, starlette_request):
+        print ("one")
+        request = await starlette_request.body()
+        print ("two")
+        data = json.loads(request)
+        print ("three")
+        print ("data", data)
+        prompt = data["prompt"]
+        max_length = data.get("max_length", 100)
+        num_return_sequences = data.get("num_sequences", 5)
+        output_biogpt = self.pipe_biogpt(
+            prompt,
+            max_length=max_length,
+            num_return_sequences=num_return_sequences,
+            do_sample=True,
+        )
+        result = output_biogpt[0]["generated_text"]
+        print ("result", result)
+        return result
+
+# 2: Deploy the deployment.
+deploy = BioGpt.bind()
+
